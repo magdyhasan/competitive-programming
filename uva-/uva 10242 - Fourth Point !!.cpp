9ee2837c13a9b98d5cf9da1ffbb3b6bd20788c14
magdy-hasan <magdy.hasan17@hotmail.com>
2017-04-02 13:57:11 +0200

    Updated comment for 10242

--- uva-/uva 10242 - Fourth Point !!.cpp
+++ uva-/uva 10242 - Fourth Point !!.cpp
@@ -13,20 +13,20 @@ int main(){
 	freopen("uva.txt", "r", stdin);
 	double ax, ay, bx, by, cx, cy;
 	while (scanf("%lf%lf", &x[0], &y[0]) != EOF){
-		for (int i = 1; i < 4; i++) scanf("%lf%lf", &x[i], &y[i]);
+		for (int i = 1; i < 4; i++) scanf("%lf%lf", &x[i], &y[i]); // read 4 points
 		st.clear();
-		for (int i = 0; i < 4; i++){
-			if (st.find({ x[i], y[i] }) != st.end()){
-				st.erase({ x[i], y[i] });
+		for (int i = 0; i < 4; i++){ // find common 2 point and put other 2 in st
+			if (st.find({ x[i], y[i] }) != st.end()){ // if this point already exist in st, this the common point
+				st.erase({ x[i], y[i] });// we don't want this common point in st
 				cx = x[i], cy = y[i];
 			}
 			else
 				st.insert({ x[i], y[i] });
 		}
-		stit = st.begin();
-		ax = stit->first, ay = stit->second; 
+		stit = st.begin();// get other 2 points from st
+		ax = stit->first, ay = stit->second; // first point
 		stit++;
-		bx = stit->first, by = stit->second;
-		printf("%.3lf %.3lf\n", ax + bx - cx, ay + by - cy);
+		bx = stit->first, by = stit->second;// second point
+		printf("%.3lf %.3lf\n", ax + bx - cx, ay + by - cy);// fourth point is just (a+b-c) because c is added twice in a and b
 	}
 }
